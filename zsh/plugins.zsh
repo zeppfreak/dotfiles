54f8a9e8df1e47d340db5e3ffd7a6edbd2ec075b
zeppfreak <zeppfreak@gmail.com>
2023-03-21 20:51:34 +0900

    comment out bat alias

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -7,6 +7,6 @@ zinit ice as"program" from"gh-r" mv"bat* -> bat" pick"bat/bat"
 zinit light sharkdp/bat
 
 # 以下はただのエイリアス設定
-if builtin command -v bat > /dev/null; then
-  alias cat="bat"
-fi
+#if builtin command -v bat > /dev/null; then
+#  alias cat="bat"
+#fi
