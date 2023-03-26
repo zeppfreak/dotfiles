d8dd5761ed3d8749901b14be46815c5beda963dc
zeppfreak <aoi@instance-1.asia-northeast1-b.c.ornate-course-263801.internal>
2023-03-26 02:12:25 +0000

    fix README.md about python3 install

--- debian/apt.sh
+++ debian/apt.sh
@@ -4,6 +4,10 @@ sudo apt-get install git-all
 # install zsh
 sudo apt-get install zsh
 
+# install python
+sudo apt-get install python3
+sudo apt-get install python3-venv 
+
 # install gcloud CLI
 sudo apt-get install apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
